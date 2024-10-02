ebe7dd319f92a432156a4d39d9c218ce3cb937ce
mlxs01 <sun.michelle.01@gmail.com>
2024-10-02 10:45:10 -0400

    Updates for comments and clean up

--- src/App.tsx
+++ src/App.tsx
@@ -17,39 +17,40 @@ const Title = styled.h1`
 `;
 
 function App() {
-    const [data, setData] = useState<Character[]>([]);
-    
-    // Fetch random dogs
-    useEffect(() => {
-        async function fetchData(): Promise<void> {
-            try {// Get 15 dogs 
-                const response = await fetch("https://dog.ceo/api/breeds/image/random/15");
-                const result = await response.json();
-                
-                // Simulate more fields for each dog (as 'id' and 'breed')
-                const formattedData = result.message.map((imgUrl: string, index: number) => ({
-                    id: index + 1,
-                    name: `Dog ${index + 1}`,
-                    image: imgUrl,
-                    breed: imgUrl.split("/")[4]  // Extract breed
-                }));
-                setData(formattedData);
-            } catch (error) {
-                console.log("There was an error fetching data: ", error);
-            }
-        }
+  const [data, setData] = useState<Character[]>([]);
+  
+  // Fetch random dogs
+  useEffect(() => {
+    async function fetchData(): Promise<void> {
+      try {
+        // Get 15 dogs 
+        const response = await fetch("https://dog.ceo/api/breeds/image/random/15");
+        const result = await response.json();
+        
+        // Simulate more fields for each dog (as 'id' and 'breed')
+        const formattedData = result.message.map((imgUrl: string, index: number) => ({
+            id: index + 1,
+            name: `Dog ${index + 1}`,
+            image: imgUrl,
+            breed: imgUrl.split("/")[4]  // Extract breed
+        }));
+        setData(formattedData);
+      } catch (error) {
+        console.log("There was an error fetching data: ", error);
+      }
+    }
 
-        fetchData();
-    }, []);
+    fetchData();
+  }, []);
 
-    return (
-      <>
-        <ParentDiv>
-            <Title>Dog Avatars</Title>
-            <Avatars data={data} />
-        </ParentDiv>
-      </>
-    );
+  return (
+    <>
+      <ParentDiv>
+        <Title>Dog Avatars</Title>
+        <Avatars data={data} />
+      </ParentDiv>
+    </>
+  );
 }
 
 export default App;
